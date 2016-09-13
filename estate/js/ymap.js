82ccd69cc49be2865e6e72e5e6c42cf0b340b4b1
fryggr <fryggr@gmail.com>
2016-09-13 16:45:38 +0300

    commit-js3

--- estate/js/ymap.js
+++ estate/js/ymap.js
@@ -149,4 +149,50 @@
 
                   myPlacemark.balloon.open();
 
+                  var myMapMobile = new ymaps.Map("YMapsID-mobile", {
+                          center: [55.67017379, 37.55885318],
+                          zoom: 14
+                      });
+
+                      myMapMobile.controls
+                      // Кнопка изменения масштаба.
+                      .add('zoomControl', { left: 5, top: 5 })
+                      // Список типов карты
+                      .add('typeSelector')
+                      // Стандартный набор кнопок
+                      .add('mapTools', { left: 35, top: 5 });
+
+                      var myPlacemarkMobile = new ymaps.Placemark([55.66973722, 37.56058648], { balloonContent: 'Тверь, Набережная Степана Разина, д. 20'}, {
+                          // Опции.
+                          hideIconOnBalloonOpen:true,
+                          // Своё изображение иконки метки.
+                          iconImageHref: 'img/map-marker.png',
+                          // Размеры метки.
+                          iconImageSize: [75, 85],
+                          // Смещение левого верхнего угла иконки относительно
+                          // её "ножки" (точки привязки).
+                          iconImageOffset: [-60, -75],
+                          // Размеры содержимого балуна
+                          balloonContentSize: [130, 130],
+                          balloonPanelMaxMapArea: 0,
+                          // Задаем макет балуна - пользовательская картинка с контентом
+                          //balloonLayout: "default#imageWithContent",
+                          // Картинка балуна
+                          //balloonImageHref: 'img/baloon.png',
+                          // Смещение картинки балуна
+                          //balloonImageOffset: [-5, -70],
+                          // Размеры картинки балуна
+                          //balloonImageSize: [381, 91],
+                          // Балун не имеет тени
+                          balloonShadow: true,
+                          ballonOpen: true
+                      });
+                      myMapMobile.behaviors.disable('scrollZoom');
+                      // Добавляем все метки на карту.
+                      myMapMobile.geoObjects.add(myPlacemarkMobile);
+
+
+
+
+
       }
