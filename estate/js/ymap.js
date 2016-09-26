5562099ab1a989d3b02c0210cfa9630d4061978d
Daria Smirnova <fryggr@gmail.com>
2016-09-26 10:41:32 +0400

    заполнение-3

--- estate/js/ymap.js
+++ estate/js/ymap.js
@@ -100,7 +100,7 @@
                   iconImageSize: [73, 95],
                   // Смещение левого верхнего угла иконки относительно
                   // её "ножки" (точки привязки).
-                  iconImageOffset: [-45, -60],
+                  iconImageOffset: [-55, -70],
                   // Размеры содержимого балуна
                   balloonContentSize: [130, 130],
                   balloonPanelMaxMapArea: 0,
