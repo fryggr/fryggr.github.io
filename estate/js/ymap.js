a797191aa9896e36d4d5ded44407ffe6f5308cef
Daria Smirnova <fryggr@gmail.com>
2016-09-26 11:46:19 +0400

    заполнение-4

--- estate/js/ymap.js
+++ estate/js/ymap.js
@@ -31,7 +31,7 @@
                   // Картинка балуна
                   balloonImageHref: 'img/baloon2.png',
                   // Смещение картинки балуна
-                  balloonImageOffset: [-5, -70],
+                  balloonImageOffset: [-35, -50],
                   // Размеры картинки балуна
                   balloonImageSize: [381, 91],
                   // Балун не имеет тени
