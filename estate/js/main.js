d33e6d1f93723c6bf777fc6d5f68c77b34b71c0c
Daria Smirnova <fryggr@gmail.com>
2016-10-12 11:18:16 +0400

    fix-24

--- estate/js/main.js
+++ estate/js/main.js
@@ -135,7 +135,18 @@ $(document).ready(function(){
     pano.stopMoving();
 
 });
+var feedbackCaptcha;
+var authCaptcha;
 
+function recaptchaCallback() {
+  feedbackCaptcha = grecaptcha.render('feedback_captcha', {
+    'sitekey' : '6LccBAkUAAAAAIWOWdJKxDiG3Ci5_y0bmCIjVJic'
+  });
+
+  authCaptcha = grecaptcha.render('auth_captcha', {
+    'sitekey' : '6LccBAkUAAAAAIWOWdJKxDiG3Ci5_y0bmCIjVJic'
+  });
+}
 // Get the modal
 var modal = document.getElementById('myModal');
 
@@ -146,10 +157,25 @@ var btn5 = document.getElementById("myBtn5");
 var btn_m = document.getElementById("myBtn-m");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
+var captcha = document.getElementsByClassName('captcha-text')[0];
 // Get the modal
 var modal2 = document.getElementById('myModal2');
-
+modal2.onsubmit = function() {
+  event.preventDefault();
+  if (grecaptcha.getResponse()!='') {
+    modal3.style.display = "block";
+    modal.style.display = "none";
+    captcha.innerHTML='Для регистрации необходимо пройти проверку на робота. Для этого подтвердите capthca.';
+  }
+}
+var form3 = document.getElementById('consult');
+form3.onsubmit = function() {
+  event.preventDefault();
+  if (grecaptcha.getResponse()!='') {
+    modal3.style.display = "block";
+    //modal.style.display = "none";
+  }
+}
 // Get the button that opens the modal
 var btn2 = document.getElementById("myBtn2");
 
@@ -172,10 +198,10 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
-btn2.onclick = function() {
-    modal.style.display = "none";
-    modal2.style.display = "block";
-}
+// btn2.onclick = function() {
+//     modal.style.display = "none";
+//     modal2.style.display = "block";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -207,25 +233,25 @@ var modal3 = document.getElementById('myModal3');
 
 
 // Get the button that opens the modal
-var btn3 = document.getElementById("myBtn3");
+var form1 = document.getElementById("myForm1");
 
 // Get the <span> element that closes the modal
 var span3 = document.getElementsByClassName("close")[2];
 // When the user clicks on the button, open the modal
-btn3.onclick = function() {
+form1.onsubmit = function() {
+  event.preventDefault();
+  if (grecaptcha.getResponse()!='') {
     modal3.style.display = "block";
+  }
 }
-btn4.onclick = function() {
-    modal3.style.display = "block";
-}
+// btn4.onclick = function() {
+//     modal3.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
 span3.onclick = function() {
-    modal3.style.display = "none";
-}
-btn3.onclick = function() {
-    modal3.style.display = "block";
 
+    modal3.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
