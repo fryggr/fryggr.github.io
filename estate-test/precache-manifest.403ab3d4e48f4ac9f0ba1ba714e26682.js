d90d11c4309b7ea1f0c7bd37ac4157a3a0fdd265
Daria Smirnova <fryggr@gmail.com>
2019-02-04 15:12:16 +0300

    first commit

---
+++ estate-test/precache-manifest.403ab3d4e48f4ac9f0ba1ba714e26682.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4fcfc6a7919393536c83",
+    "url": "/static/js/main.4fcfc6a7.chunk.js"
+  },
+  {
+    "revision": "f7251164ce0ca66ed0a9",
+    "url": "/static/js/1.f7251164.chunk.js"
+  },
+  {
+    "revision": "4fcfc6a7919393536c83",
+    "url": "/static/css/main.2b14a577.chunk.css"
+  },
+  {
+    "revision": "d5f89f90209daf557d8a4734a8d4b248",
+    "url": "/index.html"
+  }
+];
